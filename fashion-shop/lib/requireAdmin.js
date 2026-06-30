19b97240584b001732c9c3d84fc4eacb6eb07542
Mir Mezbaul Azim <116035090+mirmezbaulazim@users.noreply.github.com>
2026-06-30 09:28:07 +0600

    Delete fashion-shop directory

--- fashion-shop/lib/requireAdmin.js
+++
@@ -1,8 +0,0 @@
-import { verifyAdminToken } from "./auth";
-import { cookies } from "next/headers";
-
-export function requireAdmin() {
-  const token = cookies().get("admin_token")?.value;
-  if (!token) return null;
-  return verifyAdminToken(token);
-}
